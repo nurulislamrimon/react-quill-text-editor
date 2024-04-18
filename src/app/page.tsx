1ab5be177092bbdead506f26c9718e2db523493f
nurulislamrimon <nurulislamrimon@gmail.com>
2024-04-18 17:03:16 +0600

    format and modules added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,45 @@ import "react-quill/dist/quill.snow.css";
 export default function Home() {
   const [value, setValue] = useState("");
 
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      [{ script: "sub" }, { script: "super" }],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       hello
-      <ReactQuill theme="snow" value={value} onChange={setValue} />
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={setValue}
+        modules={modules}
+        formats={formats}
+      />
     </main>
   );
 }
