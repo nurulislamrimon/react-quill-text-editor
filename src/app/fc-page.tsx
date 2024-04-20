7acd36505b6bf2e4a86042a91cbc23badc2cb002
nurulislamrimon <nurulislamrimon@gmail.com>
2024-04-20 21:45:03 +0600

    class component added

---
+++ src/app/fc-page.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+export default function Home() {
+  const [value, setValue] = useState("");
+
+  const modules = {
+    toolbar: [
+      [{ font: [] }, { size: ["small", "large", "huge"] }],
+      // [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ color: [] }, { background: [] }],
+      [{ align: [] }],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["image", "link", "video", "formula"],
+      [{ script: "sub" }, { script: "super" }],
+      ["code-block"],
+      ["clean"],
+    ],
+  };
+
+  // const formats = [
+  //   "font",
+  //   "size",
+  //   "script",
+  //   // "header",
+  //   "bold",
+  //   "italic",
+  //   "underline",
+  //   "strike",
+  //   "blockquote",
+  //   "list",
+  //   "bullet",
+  //   "indent",
+  //   "link",
+  //   "image",
+  // ];
+
+  console.log(value);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      hello
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={setValue}
+        modules={modules}
+        // formats={formats}
+      />
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,30 @@
 "use client";
-import { useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import React, { useState } from "react";
+import QuillEditor from "./_component/Editor";
 
-export default function Home() {
+const MainPage = () => {
   const [value, setValue] = useState("");
 
-  const modules = {
-    toolbar: [
-      [{ font: [] }, { size: ["small", "large", "huge"] }],
-      // [{ header: [1, 2, false] }],
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [{ color: [] }, { background: [] }],
-      [{ align: [] }],
-      [
-        { list: "ordered" },
-        { list: "bullet" },
-        { indent: "-1" },
-        { indent: "+1" },
-      ],
-      ["image", "link", "video", "formula"],
-      [{ script: "sub" }, { script: "super" }],
-      ["code-block"],
-      ["clean"],
-    ],
+  const onEditorChange = (data) => {
+    // Handle the updated HTML content
+    setValue(data);
+    console.log("Editor content changed:", data);
   };
 
-  // const formats = [
-  //   "font",
-  //   "size",
-  //   "script",
-  //   // "header",
-  //   "bold",
-  //   "italic",
-  //   "underline",
-  //   "strike",
-  //   "blockquote",
-  //   "list",
-  //   "bullet",
-  //   "indent",
-  //   "link",
-  //   "image",
-  // ];
+  const onFilesChange = (files) => {
+    // Handle the uploaded files
+    console.log("Uploaded files:", files);
+  };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello
-      <ReactQuill
-        theme="snow"
-        value={value}
-        onChange={setValue}
-        modules={modules}
-        // formats={formats}
+    <div>
+      <QuillEditor
+        placeholder="write something!"
+        onEditorChange={onEditorChange}
+        onFilesChange={onFilesChange}
       />
-    </main>
+    </div>
   );
-}
+};
+
+export default MainPage;
